5f8b50b6ad37ba08f43acefdb31cb1fcc852b7ca
Adnan Zaki <zaki.adnan6@gmail.com>
2024-04-14 11:54:01 +0530

    Started Frontend Work.

---
+++ Frontend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/healthcheck")
+def healthcheck():
+    return render_template("healthcheck.html")
+
+@app.route("/additem")
+def additem():
+    return render_template("additem.html")
+
+@app.route("/stockmanagement")
+def stockmanagement():
+    return render_template("stockmanagement.html")
+
+@app.route("/addorder")
+def addorder():
+    return render_template("addorder.html")
+
+@app.route("/ordertracking")
+def ordertracking():
+    return render_template("ordertracking.html")
+
+app.run(port=8000, debug=True)
